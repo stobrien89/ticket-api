848f988cbea05c273ed99d8ae381a33f9c77e566
stobrien89 <stobrien@crimson.ua.edu>
2021-10-14 19:34:25 -0700

    add model

---
+++ models/ticket.js
@@ -0,0 +1,10 @@
+const { model, Schema } = require('mongoose');
+
+const ticketSchema = new Schema ({
+    title: String,
+    source: String,
+    assignee: String,
+    tags: [{ type: String }] 
+});
+
+module.exports = model('Ticket', ticketSchema);
