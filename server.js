fb3f79ab93e291e78997e5740821692b6023d02f
stobrien89 <stobrien@crimson.ua.edu>
2021-10-14 18:56:17 -0700

    mongoose connected

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const cors = require('cors');
 const PORT = process.env.PORT || 8000;
+
+//Mongoose connection
+const MONGO_URI = process.env.MONGO_URI;
+const db = mongoose.connection;
+
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+db.on('open', () => {
+    console.log('Mongo is connected')
+})
+
 
 app.use(express.json());
 app.use(cors());
