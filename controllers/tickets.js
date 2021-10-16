cfbfbf0a677e397fbece671353d85c6383a5830f
stobrien89 <stobrien@crimson.ua.edu>
2021-10-16 15:12:57 -0700

    add delete endpoint

--- controllers/tickets.js
+++ controllers/tickets.js
@@ -40,4 +40,16 @@ router.put('/:id', async (req, res) => {
     }
 })
 
+//Delete endpoint
+router.delete('/:id', async (req, res) => {
+    try{
+        const deletedTicket = await Ticket.findByIdAndDelete(req.params.id);
+        res.status(200).json(deletedTicket);
+    }catch(err) {
+        console.error(err);
+        res.status(400).json({
+            message: error.message
+        })
+    }
+})
 module.exports = router;
